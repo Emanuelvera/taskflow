dad4b0e99823170bef91b81aa3e73804a41631af
matiassaunig <matias.saunig@mi.unc.edu.ar>
2026-04-08 23:28:14 -0300

    Se modifico Program.cs para probar si crea la tarea correctamente... Crea la tarea OK

--- CrearTarea/CreateTask/CreateTask/Program.cs
+++ CrearTarea/CreateTask/CreateTask/Program.cs
@@ -4,7 +4,12 @@
     {
         private static void Main(string[] args)
         {
-            
+            var tareaService = new TaskService();
+
+            Console.WriteLine("Ingrese una descripción de la tarea:");
+            string descripcion = Console.ReadLine();
+
+            tareaService.CrearTarea(descripcion);
         }
     }
 }
