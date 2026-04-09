c3e50bb451213347104f7e826eef5b71d8b949c4
GL-1005 <mguada.loyola@mi.unc.edu.ar>
2026-04-09 14:29:22 -0300

    feat: se creo estructura base y modelo Tarea

---
+++ ListarTareas/Tareas.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ListarTareas
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public bool Completada { get; set; }
+    }
+}
