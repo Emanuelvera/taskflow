f8789acbde5c195e0a79192f268c17316eb14dbd
matiassaunig <matias.saunig@mi.unc.edu.ar>
2026-04-08 23:25:27 -0300

    Se creo clase TaskService

---
+++ CrearTarea/CreateTask/CreateTask/TaskService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CreateTask
+{
+    public class TaskService
+    {
+        private List<Task> tareas = new List<Task>();
+        private int nextId = 1;
+
+        public void CrearTarea(string descripcion)
+        {
+            var tarea = new Task
+            {
+                Id = nextId++,
+                Descripcion = descripcion,
+                Completada = false
+            };
+
+            tareas.Add(tarea);
+
+            Console.WriteLine("Tarea creada correctamente");
+        }
+    }
+}
