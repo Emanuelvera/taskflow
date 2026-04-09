ec6490638943414d7a62af06484c728fd0bf9f0f
marianelachumbita <marianela.chumbita@mi.unc.edu.ar>
2026-04-09 11:07:37 -0300

    Se creo clase tarea service y tarea, tarea con estructura

---
+++ EstadoTarea/TaskStatus/TaskStatus/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class TaskStatus
+
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ EstadoTarea/TaskStatus/TaskStatus/Tarea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskStatus
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public bool Completada { get; set; }
+    }
+}
