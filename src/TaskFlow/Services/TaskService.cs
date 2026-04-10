3e6bbbc9abe98947aa3f0e2a1de3bbb68d4a2978
marianelachumbita <marianela.chumbita@mi.unc.edu.ar>
2026-04-10 11:03:22 -0300

    Se saco cancele del taskstatus. Se completo la funcion actualizar estado.(linea 14: falta agregar funcion de listar tarea.)

--- src/TaskFlow/Services/TaskService.cs
+++ src/TaskFlow/Services/TaskService.cs
@@ -2,26 +2,10 @@
 using System.Collections.Generic;
 using TaskFlow.Models;
 
-namespace CreateTask
+namespace TaskFlow.Services
 {
     public class TaskService
     {
-        private List<TaskItem> tareas = new List<TaskItem>();
-        private int nextId = 1;
-
-        public void CrearTarea(string descripcion)
-        {
-            var tarea = new TaskItem
-            {
-                ID = nextId++,
-                Title = descripcion,
-                Description = descripcion,
-                Status = TaskFlow.Models.TaskStatus.Pending
-            };
-
-            tareas.Add(tarea);
-
-            Console.WriteLine("Tarea creada correctamente");
-        }
+        
     }
 }
